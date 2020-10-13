b5a03a080e1e5875ed9e0b7581820a5665db16ac
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-10-13 18:16:31 +0900

    first commit

---
+++ app/src/main/java/com/example/location_finder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.location_finder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
